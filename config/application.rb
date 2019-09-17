c07f3d10600b30a387521fd6b1308ec35db33074
まっく <mackxxx@makkunoMacBook-Pro.local>
2019-09-18 01:11:36 +0900

    テストコード追加

--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,14 @@ module Amazonlist2
       g.stylesheets false  
       g.javascripts false   
       g.helper false       
-      g.test_framework false 
+      g.test_framework :rspec,
+                        fixtures: true,
+                        view_specs: false,
+                        helper_specs: false,
+                        routing_specs: false,
+                        controller_specs: true,
+                        request_specs: false
+      g.fixture_replacement :factory_bot, dir: "spec/factories"
     end
   end
 end
