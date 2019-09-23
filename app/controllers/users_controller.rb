9b8f893922a03487e3d05596e0158059875ad371
まっく <mackxxx@makkunoMacBook-Pro.local>
2019-09-23 17:20:25 +0900

    view修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,10 +11,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-    redirect_to @user, success:  "登録に成功しました。"
+      redirect_to @user, success:  "登録に成功しました。"
     else
-    flash.now[:danger] = 'ユーザの登録に失敗しました。'
-    render :new
+      flash.now[:danger] = 'ユーザの登録に失敗しました。'
+      render :new
     end
   end
 
@@ -26,6 +26,6 @@ class UsersController < ApplicationController
 
   private
     def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
   end
