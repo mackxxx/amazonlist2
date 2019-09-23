0cca7972b9153d0cd9780fba20c717f8e8d3862e
まっく <mackxxx@makkunoMacBook-Pro.local>
2019-09-23 22:32:42 +0900

    test修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,59 +2,57 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-  before do
-    @user = FactoryBot.build(:user)
-  end
+  let(:user) { build :user }
 
-  context "when user is valid" do
-    it "値が入力されている" do
-      expect(@user).to be_valid
+  context "値が入力されている場合" do
+    it "ユーザ登録は有効" do
+      expect(user).to be_valid
     end
   end
 
-  context "when user is invalid" do
-    it "値が空白" do
-      @user.name = "  "
-      expect(@user).to be_invalid
+  context "値が空白の場合" do
+    it "ユーザ登録は無効" do
+      user.name = nil
+      expect(user).to be_invalid
     end
   end
 
-  context "when email format is invalid" do
-    it "emailのvalidateが正しく機能しているか" do
+  context "無効なEメールを登録しようとした場合" do
+    it "emailのvalidateが正しく機能している" do
       address = %w[user@example,com user_at_foo.org user.name@example.foo@bar_baz.com foo@bar+baz.com foo@bar..com]
       address.each do |invalid_address|
-        expect(FactoryBot.build(:user, email: invalid_address)).to be_invalid
+        expect(build(:user, email: invalid_address)).to be_invalid
       end
     end
   end
 
-  context "when email addresses should be unique" do
-    it "一意性が正しく機能しているか" do
-      duplicate_user = @user.dup
-      duplicate_user.email = @user.email.upcase
-      @user.save!
+  context "Eメールが重複している場合" do
+    it "一意性が正しく機能する" do
+      duplicate_user = user.dup
+      duplicate_user.email = user.email.upcase
+      user.save!
       expect(duplicate_user).to be_invalid
     end
   end
 
-  it "emailを小文字に変換後の値と大文字を混ぜて登録されたアドレスが同じか" do
-    @user.email = "Foo@ExAMPle.CoM"
-    @user.save!
-    expect(@user.reload.email).to eq "foo@example.com"
+  it "emailを小文字に変換後の値と大文字を混ぜて登録されたアドレスが同じになる" do
+    user.email = "Foo@ExAMPle.CoM"
+    user.save!
+    expect(user.reload.email).to eq "foo@example.com"
   end
 
-  describe "password length" do
+  describe "パスワードの長さ" do
     context "パスワードが６桁の時" do
       it "正しい" do
-        @user = FactoryBot.build(:user, password: "a" * 6, password_confirmation: "a" * 6)
-        expect(@user).to be_valid
+        user = build(:user, password: "a" * 6, password_confirmation: "a" * 6)
+        expect(user).to be_valid
       end
     end
 
     context "パスワードが５桁の時" do
       it "正しくない" do
-        @user = FactoryBot.build(:user, password: "a" * 5, password_confirmation: "a" * 5)
-        expect(@user).to be_invalid
+        user = build(:user, password: "a" * 5, password_confirmation: "a" * 5)
+        expect(user).to be_invalid
       end
     end
   end
--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -2,6 +2,5 @@ require 'rails_helper'
 
 RSpec.describe "User", type: :system do
   it  do
-    
   end
 end
