9211dac256f29f6d7932f163a68a67147fc54d1c
まっく <mackxxx@makkunoMacBook-Pro.local>
2019-10-02 02:57:11 +0900

    navbar　編集

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 require("jquery").start()
 require("jquery_ujs").start()
+require ("popper")
 require("bootstrap-sprockets")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: "toppages#index"
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
-  delete 'login', to: 'sessions#destroys'
+  delete 'logout', to: 'sessions#destroys'
   get 'signup', to: 'users#new'
   resources :users, only:[:show, :create, :edit, :update] 
 end
