1902974e3475ebfc0a663373d0a17b7732bc95ea
まっく <mackxxx@makkunoMacBook-Pro.local>
2019-10-01 16:48:17 +0900

    contoroller destroy以外

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,15 @@
 class SessionsController < ApplicationController
+  
   def new
   end
 
   def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+    else
+      flash.now[:danger] = 'メールとパスワードの組み合わせが間違っています！'
+      render 'new'
+    end
   end
 
   def destroy
