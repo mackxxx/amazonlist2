fd2c832fb73518dbeabdf6bc980a926a7c9e52ef
まっく <mackxxx@makkunoMacBook-Pro.local>
2019-10-01 19:03:50 +0900

    view修正前

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   add_flash_types :success, :danger
-end
+  
+  include SessionsHelper
+
+  private
+    def require_user_logged_in
+      if not logged_in?
+        redirect_to login_url
+      end
+    end
+  end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-
+  before_action :require_user_logged_in, only: [:show]
+  
   def show
     @user = User.find(params[:id])
   end
@@ -11,6 +12,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       redirect_to @user, success:  "登録に成功しました。"
     else
       flash.now[:danger] = 'ユーザの登録に失敗しました。'
