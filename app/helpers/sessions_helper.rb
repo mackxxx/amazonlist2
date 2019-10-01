3917a8c7cb2c0a2d8e93a058c3c7202033ef6f6c
まっく <mackxxx@makkunoMacBook-Pro.local>
2019-10-01 19:47:00 +0900

    Nil class 対応前

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,4 +1,4 @@
-modulue SessionsHelper
+module SessionsHelper
 
   def log_in(user)
     session[:user_id] = user.id
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root to: "toppages#index"
   get 'login', to: 'sessions#new'
-  get 'login', to: 'sessions#create'
-  get 'login', to: 'sessions#destroys'
+  post 'login', to: 'sessions#create'
+  delete 'login', to: 'sessions#destroys'
   get 'signup', to: 'users#new'
   resources :users, only:[:show, :create, :edit, :update] 
 end
