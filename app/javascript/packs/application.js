4b91d2ba2bb3399e020bf6fffe57d69173d571a8
まっく <mackxxx@makkunoMacBook-Pro.local>
2019-10-05 19:54:31 +0900

    flashmessage出現

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,6 @@
 import 'bootstrap'
 import '@fortawesome/fontawesome-free/js/all'
 import '../src/application.scss'
-require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
