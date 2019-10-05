ab8a0e1afb01c08a8774260d1c35bad0ecddced6
まっく <mackxxx@makkunoMacBook-Pro.local>
2019-10-06 00:41:17 +0900

    jquery

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,9 +1,8 @@
-import 'bootstrap'
-import '@fortawesome/fontawesome-free/js/all'
-import '../src/application.scss'
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("jquery").start()
-require("rails-ujs").start()
+require("jquery")
+import "bootstrap"
+import "../src/application.scss"
+import "@fortawesome/fontawesome-free/js/all"
