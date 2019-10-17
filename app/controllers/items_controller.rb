89ba03451b61bd49394dbb273ec35eabe34e96b8
まっく <mackxxx@makkunoMacBook-Pro.local>
2019-10-18 02:40:42 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,3 @@
-require "item.rb"
-
 class ItemsController < ApplicationController
   def index
     @items = []
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,18 +3,18 @@ class Item < ApplicationRecord
   validates :name, presence: true, length: { maximum: 255 }
   validates :url, presence: true, length: { maximum: 255 }
   validates :image_url, presence: true, length: { maximum: 255 }
-  
-    def read(result)
-      code = result['itemCode']
-      name = result['itemName']
-      url = result['itemUrl']
-      image_url = result['mediumImageUrls'].first.gsub('?_ex=128x128', '')
 
-      {
-        code: code,
-        name: name,
-        url: url,
-        image_url: image_url,
-      }
-    end
+  def read(result)
+    code = result['itemCode']
+    name = result['itemName']
+    url = result['itemUrl']
+    image_url = result['mediumImageUrls'].first.gsub('?_ex=128x128', '')
+
+    {
+      code: code,
+      name: name,
+      url: url,
+      image_url: image_url,
+    }
   end
+end
