5f7f8330aba9675d25348f0af48c036efba20fb5
まっく <mackxxx@makkunoMacBook-Pro.local>
2019-10-26 23:17:46 +0900

    rb33

--- config/application.rb
+++ config/application.rb
@@ -28,15 +28,8 @@ module Amazonlist2
     config.assets.initialize_on_precompile=false
     config.generators do |g|
       g.stylesheets false  
-      g.javascripts false   
+      g.javascripts false
       g.helper false       
-      g.test_framework :rspec,
-                        fixtures: false,
-                        view_specs: false,
-                        helper_specs: false,
-                        routing_specs: false,
-                        controller_specs: false,
-                        request_specs: false
       g.fixture_replacement :factory_bot, dir: "spec/factories"
     end
   end
