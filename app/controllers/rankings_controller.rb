cc33bcf539580479f22df3c8f55dfbf4555609a9
mackxxx <42348939+mackxxx@users.noreply.github.com>
2019-12-19 00:14:37 +0900

    testコード前の修正

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -1,11 +1,11 @@
 class RankingsController < ApplicationController
   def want
     @ranking_counts = Want.ranking
-    @items = Item.find(@ranking_counts.keys)
+    @items = Item.where id:(@ranking_counts.keys)
   end
   
   def desire
     @ranking_counts = Desire.ranking
-    @items = Item.find(@ranking_counts.keys)
+    @items = Item.where id:(@ranking_counts.keys)
   end
 end
