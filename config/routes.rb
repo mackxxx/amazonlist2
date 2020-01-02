7f16472d9180bfd1bd2d0f7986e2260325665005
mackxxx <42348939+mackxxx@users.noreply.github.com>
2020-01-02 21:51:53 +0900

    route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: "toppages#index"
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
-  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/:provider/callback', to: 'omniauth#callback'
   delete 'logout', to: 'sessions#destroy'
   get 'signup', to: 'users#new'
   get 'rankings/want', to: 'rankings#want'
